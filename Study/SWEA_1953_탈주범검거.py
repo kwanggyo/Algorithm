073bbdd483fd4794ea392a27f118d0eeeb04ac85
kwanggyo <dlrhkdry29@gmail.com>
2021-06-08 01:27:57 +0900

    0608

--- Study/SWEA_1953_탈주범검거.py
+++ Study/SWEA_1953_탈주범검거.py
@@ -1,4 +1,4 @@
-4# 1부터 시작(시간)
+# 1부터 시작(시간)
 # 구조물 타입에 따라 갈 수 있는 방향을 정한다.
 # 도착했던 곳은 방문표시, cnt
 # 1 -> 상하좌우
@@ -8,8 +8,8 @@
 # 5 -> 우하
 # 6 -> 좌하
 # 7 -> 상좌
-############### 서로 모양을 생각해야함 ㅡ와 ㅣ 는 연결 불가 !
-# 1. 각각 연결가능할 때 cnt를 추가하는 방식 -> 조건이 너무 많다..
+# 서로 모양을 생각해야함 ㅡ와 ㅣ 는 연결 불가 !
+# 각각 연결가능할 때 cnt를 추가하는 방식 -> 조건이 너무 많다..
 
 def dfs(r, c, hour):
     if hour == L:
@@ -19,56 +19,106 @@ def dfs(r, c, hour):
             nr = r + dr[k]
             nc = c + dc[k]
             if 0 <= nr < N and 0 <= nc < M and underground[nr][nc] != 0 and visited[nr][nc] == 0:
-                    visited[nr][nc] = 1
-                    dfs(nr, nc, hour + 1)
+                if k == 0:
+                    if underground[nr][nc] == 1 or underground[nr][nc] == 2 or underground[nr][nc] == 5 or underground[nr][nc] == 6:
+                        visited[nr][nc] = 1
+                        dfs(nr, nc, hour + 1)
+                elif k == 1:
+                    if underground[nr][nc] == 1 or underground[nr][nc] == 3 or underground[nr][nc] == 6 or underground[nr][nc] == 7:
+                        visited[nr][nc] = 1
+                        dfs(nr, nc, hour + 1)
+                elif k == 2:
+                    if underground[nr][nc] == 1 or underground[nr][nc] == 2 or underground[nr][nc] == 4 or underground[nr][nc] == 7:
+                        visited[nr][nc] = 1
+                        dfs(nr, nc, hour + 1)
+                else:
+                    if underground[nr][nc] == 1 or underground[nr][nc] == 3 or underground[nr][nc] == 4 or underground[nr][nc] == 5:
+                        visited[nr][nc] = 1
+                        dfs(nr, nc, hour + 1)
     elif underground[r][c] == 2:
         for k in range(4):
             if k == 0 or k == 2:    # 상 하
                 nr = r + dr[k]
                 nc = c + dc[k]
                 if 0 <= nr < N and 0 <= nc < M and underground[nr][nc] != 0 and visited[nr][nc] == 0:
-                        visited[nr][nc] = 1
-                        dfs(nr, nc, hour + 1)
+                    if k == 0:
+                        if underground[nr][nc] == 1 or underground[nr][nc] == 2 or underground[nr][nc] == 5 or underground[nr][nc] == 6:
+                            visited[nr][nc] = 1
+                            dfs(nr, nc, hour + 1)
+                    elif k == 2:
+                        if underground[nr][nc] == 1 or underground[nr][nc] == 2 or underground[nr][nc] == 4 or underground[nr][nc] == 7:
+                            visited[nr][nc] = 1
+                            dfs(nr, nc, hour + 1)
     elif underground[r][c] == 3:
         for k in range(4):
             if k == 1 or k == 3:    # 좌 우
                 nr = r + dr[k]
                 nc = c + dc[k]
                 if 0 <= nr < N and 0 <= nc < M and underground[nr][nc] != 0 and visited[nr][nc] == 0:
-                        visited[nr][nc] = 1
-                        dfs(nr, nc, hour + 1)
+                    if k == 1:
+                        if underground[nr][nc] == 1 or underground[nr][nc] == 3 or underground[nr][nc] == 6 or underground[nr][nc] == 7:
+                            visited[nr][nc] = 1
+                            dfs(nr, nc, hour + 1)
+                    elif k == 3:
+                        if underground[nr][nc] == 1 or underground[nr][nc] == 3 or underground[nr][nc] == 4 or underground[nr][nc] == 5:
+                            visited[nr][nc] = 1
+                            dfs(nr, nc, hour + 1)
     elif underground[r][c] == 4:
         for k in range(4):
             if k == 0 or k == 1:    # 상 우
                 nr = r + dr[k]
                 nc = c + dc[k]
                 if 0 <= nr < N and 0 <= nc < M and underground[nr][nc] != 0 and visited[nr][nc] == 0:
-                        visited[nr][nc] = 1
-                        dfs(nr, nc, hour + 1)
+                    if k == 0:
+                        if underground[nr][nc] == 1 or underground[nr][nc] == 2 or underground[nr][nc] == 5 or underground[nr][nc] == 6:
+                            visited[nr][nc] = 1
+                            dfs(nr, nc, hour + 1)
+                    elif k == 1:
+                        if underground[nr][nc] == 1 or underground[nr][nc] == 3 or underground[nr][nc] == 6 or underground[nr][nc] == 7:
+                            visited[nr][nc] = 1
+                            dfs(nr, nc, hour + 1)
     elif underground[r][c] == 5:
         for k in range(4):
             if k == 1 or k == 2:    # 우 하
                 nr = r + dr[k]
                 nc = c + dc[k]
                 if 0 <= nr < N and 0 <= nc < M and underground[nr][nc] != 0 and visited[nr][nc] == 0:
-                        visited[nr][nc] = 1
-                        dfs(nr, nc, hour + 1)
+                    if k == 1:
+                        if underground[nr][nc] == 1 or underground[nr][nc] == 3 or underground[nr][nc] == 6 or underground[nr][nc] == 7:
+                            visited[nr][nc] = 1
+                            dfs(nr, nc, hour + 1)
+                    elif k == 2:
+                        if underground[nr][nc] == 1 or underground[nr][nc] == 2 or underground[nr][nc] == 4 or underground[nr][nc] == 7:
+                            visited[nr][nc] = 1
+                            dfs(nr, nc, hour + 1)
     elif underground[r][c] == 6:
         for k in range(4):
             if k == 2 or k == 3:    # 하 좌
                 nr = r + dr[k]
                 nc = c + dc[k]
                 if 0 <= nr < N and 0 <= nc < M and underground[nr][nc] != 0 and visited[nr][nc] == 0:
-                        visited[nr][nc] = 1
-                        dfs(nr, nc, hour + 1)
+                    if k == 2:
+                        if underground[nr][nc] == 1 or underground[nr][nc] == 2 or underground[nr][nc] == 4 or underground[nr][nc] == 7:
+                            visited[nr][nc] = 1
+                            dfs(nr, nc, hour + 1)
+                    elif k == 3:
+                        if underground[nr][nc] == 1 or underground[nr][nc] == 3 or underground[nr][nc] == 4 or underground[nr][nc] == 5:
+                            visited[nr][nc] = 1
+                            dfs(nr, nc, hour + 1)
     elif underground[r][c] == 7:
         for k in range(4):
             if k == 0 or k == 3:    # 상 좌
                 nr = r + dr[k]
                 nc = c + dc[k]
                 if 0 <= nr < N and 0 <= nc < M and underground[nr][nc] != 0 and visited[nr][nc] == 0:
-                        visited[nr][nc] = 1
-                        dfs(nr, nc, hour + 1)
+                    if k == 0:
+                        if underground[nr][nc] == 1 or underground[nr][nc] == 2 or underground[nr][nc] == 5 or underground[nr][nc] == 6:
+                            visited[nr][nc] = 1
+                            dfs(nr, nc, hour + 1)
+                    elif k == 3:
+                        if underground[nr][nc] == 1 or underground[nr][nc] == 3 or underground[nr][nc] == 4 or underground[nr][nc] == 5:
+                            visited[nr][nc] = 1
+                            dfs(nr, nc, hour + 1)
 
 
 T = int(input())
@@ -86,7 +136,5 @@ for tc in range(1, T + 1):
         for j in range(M):
             if visited[i][j] == 1:
                 ans += 1
-    print(visited)
+    # print(visited)
     print('#{} {}'.format(tc, ans))
-
-
