57102b2809c4b731a35e6be296fa76276c6ab608
kwanggyo <dlrhkdry29@gmail.com>
2021-06-17 03:49:54 +0900

    0617

--- Study/BJ_16918_봄버맨.py
+++ Study/BJ_16918_봄버맨.py
@@ -4,22 +4,30 @@
 # 비어있는 곳에 폭탄을 설치하는 함수
 # 폭탄을 터트리는 함수
 
-def search_bumb(map):
+# 31756KB, 3416ms
+def search_bumb(map):   # 폭탄 찾기
     for r in range(R):
         for c in range(C):
             if map[r][c] == 'O':
                 bumb_li.append((r, c))
 
-def make_bumb(map):
+
+def make_bumb(map):     # 폭탄 만들기
     for r in range(R):
         for c in range(C):
             map[r][c] = 'O'
 
-def bumbbumb(bumb_li, map):
-    r, c = bumb_li.pop()
-    for k in range(4):
-        nr = r + dr[k]
-        nc = c + dc[k]
+
+def bumbbumb(bumb_li, map):     # 폭탄 터트리기
+    while bumb_li:
+        r, c = bumb_li.pop()
+        map[r][c] = '.'
+        for k in range(4):
+            nr = r + dr[k]
+            nc = c + dc[k]
+            if 0 <= nr < R and 0 <= nc < C:
+                map[nr][nc] = '.'
+
 
 R, C, N = map(int, input().split())
 bumb_map = [list(input()) for _ in range(R)]
@@ -28,6 +36,21 @@ bumb_li = []
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
-cnt = 0
-while cnt < N:
-    pass
+cnt = 1
+while True:
+    if cnt >= N:
+        break
+    search_bumb(bumb_map)
+    make_bumb(bumb_map)
+    cnt += 1
+    if cnt >= N:
+        break
+    bumbbumb(bumb_li, bumb_map)
+    cnt += 1
+    if cnt >= N:
+        break
+
+result = ''
+for i in range(R):
+    result = ''.join(bumb_map[i])
+    print(result)
