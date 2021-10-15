56cbaf22c5de80df9f5971ecdcd0a5ca65761959
kwanggyo <dlrhkdry29@gmail.com>
2021-10-15 17:11:13 +0900

    1015

---
+++ Study/BJ_1106_호텔.py
@@ -0,0 +1,28 @@
+# https://www.acmicpc.net/problem/1106
+# 1. N이 큰 순서대로 정렬 후
+# 2. 첫번째 인덱스 값으로 목표 N을 나눈 후 몫부터 1씩 내려오며 가능한 조합을 구한다.
+# 3. 그 조합의 비용을 구하여 비교한다.
+
+import sys
+input = sys.stdin.readline
+C, N = map(int, input().split())
+hotel = []
+ans = []
+
+for _ in range(N):
+    c, n = map(int, input().split())
+    hotel.append((c, n))
+hotel.sort(key=lambda x : x[1], reverse=True)
+# hotel2 = sorted(hotel, key=lambda x:x[1], reverse=True)
+div = C // hotel[0][1]
+remainder = C - div * hotel[0][0]
+cost = div * hotel[0][0]
+
+def combination(div, remainder, cost):
+    if remainder == 0:
+        ans.append(cost)
+        return
+    if remainder < 0:
+        return
+    for i in range(1, N):
+        # 잘못된 풀이 -> DP로 해야한다
