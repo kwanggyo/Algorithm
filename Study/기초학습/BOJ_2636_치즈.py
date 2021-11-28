f67f2d77ef3d9b3a48c9bd7e4aeaed8c37e8e3f9
kwanggyo <dlrhkdry29@gmail.com>
2021-11-28 19:41:42 +0900

    BOJ 2636 : 치즈

--- Study/기초학습/BOJ_2636_치즈.py
+++ Study/기초학습/BOJ_2636_치즈.py
@@ -1,4 +1,5 @@
-https://www.acmicpc.net/problem/2636
+# https://www.acmicpc.net/problem/2636
+# 32116KB 136ms
 
 from collections import deque
 import sys
@@ -7,14 +8,29 @@ input = sys.stdin.readline
 
 
 def bfs():
+    check = [[0] * M for _ in range(N)]
+    Q = deque()
+    Q.append((0, 0))
+    check[0][0] = 1
+    cnt = 0
     while Q:
-        return
+        r, c = Q.popleft()
+        for k in range(4):
+            nr = r + dr[k]
+            nc = c + dc[k]
+            if 0 <= nr < N and 0 <= nc < M and check[nr][nc] == 0:
+                if cheese[nr][nc] == 0:
+                    check[nr][nc] = 1
+                    Q.append((nr, nc))
+                else:
+                    check[nr][nc] = 1
+                    cheese[nr][nc] = 0
+                    cnt += 1
+    return cnt
 
 
 N, M = map(int, input().split())
-cheeze = [list(map(int, input().split())) for _ in range(N)]
-check = [[0] * M for _ in range(N)]
-Q = deque()
+cheese = [list(map(int, input().split())) for _ in range(N)]
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 result = []
@@ -25,4 +41,5 @@ while True:
         break
     result.append(count)
 
+print(len(result))
 print(result[-1])
