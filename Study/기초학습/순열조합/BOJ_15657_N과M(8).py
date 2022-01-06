faa0dcb8fd49aab0a21378952398f7ce66ca4aa5
kwanggyo <dlrhkdry29@gmail.com>
2022-01-06 17:26:00 +0900

    BOJ 15657 : N과 M(8) : 중복 조합

---
+++ Study/기초학습/순열조합/BOJ_15657_N과M(8).py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/15657
+# # 30864KB, 76ms
+# # 중복 조합
+
+
+from itertools import combinations_with_replacement
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+for i in combinations_with_replacement(arr, M):
+    print(' '.join(map(str, i)))
