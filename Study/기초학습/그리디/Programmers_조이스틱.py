216cfaae398863ec146d8d7441bc0f645884e35f
kwanggyo <dlrhkdry29@gmail.com>
2022-01-19 15:01:04 +0900

    Programmers Lv.2 : 조이스틱 : 그리디 : 안되는 반례 찾아보기

--- Study/기초학습/그리디/Programmers_조이스틱.py
+++ Study/기초학습/그리디/Programmers_조이스틱.py
@@ -1,31 +1,29 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42860
 # 그리디
 # A : 65, Z : 90
+# 74.1점 -> 실패하는 테스트케이스가 있음
 
 def solution(name):
     answer = 0
+    min_move = len(name) - 1    # 좌 -> 우로 끝까지 갈 때
+
+    # 끝이 A로 끝나는 경우
+    while name[min_move] == 'A' and min_move > 0:
+        min_move -= 1
+
+    # 전부 A인 경우
+    if (min_move < 0):
+        return answer
+
     for idx, x in enumerate(name):
-        if ord(x) == 65:
-            answer += 1
-            continue
-        # 위로 올리면서 바꾼 횟수가 아래로 내리면서 바꾼 횟수보다 작으면
-        elif ord(x) - ord('A') < ord('Z') - ord(x) + 1:
-            answer += ord(x) - 65
-        # 그렇지 않으면
-        else:
-            answer += 91 - ord(x)
+        # 위, 아래로 움직이는 횟수
+        answer += min(ord(x) - ord('A'), ord('Z') - ord(x) + 1)
 
+        # 좌우로 움직이는 횟수
+        next = idx + 1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+        min_move = min(min_move, idx + (idx + len(name)) - next)
+
+    answer += min_move
     return answer
-
-print(solution("JEROEN"))
-print(solution("JAN"))
-
-
-
-'''
-A B C D E F
-G H I J K L
-M N O P Q R
-S T U V W X
-Y Z
-'''
