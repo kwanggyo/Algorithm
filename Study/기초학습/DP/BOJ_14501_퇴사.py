6a9feaed70a470ebefa64a816c41f8f1fe08b3be
kwanggyo <dlrhkdry29@gmail.com>
2022-02-02 18:26:20 +0900

    BOJ 14501 : 퇴사 : DP

--- Study/기초학습/DP/BOJ_14501_퇴사.py
+++ Study/기초학습/DP/BOJ_14501_퇴사.py
@@ -1,13 +1,23 @@
 # https://www.acmicpc.net/problem/14501
 # DP
+# 30864KB, 68ms
 
 import sys
 input = sys.stdin.readline
 
 dp = [0] * 16
 N = int(input())
+T = [0] * N
 P = [0] * N
-for _ in range(N):
+for k in range(N):
     t, p = map(int, input().split())
-    P[t] = p
+    T[k] = t
+    P[k] = p
 
+for i in range(N - 1, -1, -1):
+    if T[i] + i > N:
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(dp[i + 1], P[i] + dp[i + T[i]])
+
+print(dp[0])
