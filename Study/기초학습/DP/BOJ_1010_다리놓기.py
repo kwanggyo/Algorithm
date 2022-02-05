7648bb75226a5bd3886597e8afdfab9f44264261
kwanggyo <dlrhkdry29@gmail.com>
2022-02-05 23:45:13 +0900

    BOJ 1010 : 다리 놓기 : DP

---
+++ Study/기초학습/DP/BOJ_1010_다리놓기.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/1010
+# DP
+# 30860KB, 68ms
+
+import sys
+input = sys.stdin.readline
+
+def factorial(n):
+    num = 1
+    for i in range(1, n + 1):
+        num *= i
+    return num
+
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    bridge = factorial(M) // (factorial(N) * factorial(M - N))
+    print(bridge)
