5914b63b4bfa5577045ec1c8dec222ed17b41560
kwanggyo <dlrhkdry29@gmail.com>
2022-02-11 23:07:59 +0900

    BOJ 9663 : N-Queen : Backtracking : 시간초과 : 다시 풀어보기

--- Study/기초학습/Backtracking/BOJ_9663_N-Queen.py
+++ Study/기초학습/Backtracking/BOJ_9663_N-Queen.py
@@ -1,26 +1,32 @@
 # https://www.acmicpc.net/problem/9663
 # Backtracking
+# 안되는 경우 -> 1. 같은 열에 다른 퀸, 2. 왼쪽 대각선이나 오른쪽 대각선에 다른 퀸(위에 체스판만 확인하면 됨)
+# 해당하는 경우를 식으로 표현
+# 3% 이후, 시간초과
 
-def find_Queen(idx, r, c, arr):
+def is_possible(x):
+    # 이전에 놓았던 퀸 위치를 가져와서 row와 column 차이가 같으면 안됨
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
+            return False
+    return True
+
+
+def find_Queen(idx):
     global ans
     if idx == N:
         ans += 1
-        return
-    for i in range(N):
-        nr = r + 1
-        nc = c + i
-        for pick_r, pick_c in arr:
-            if pick_c - 1 == nc or pick_c + 1 == nc:
-                continue
-            arr.append((nr, nc))
-            find_Queen(idx + 1, nr, nc, arr)
-            arr.pop()
-    return
-
-
+        # return  # return을 안쓸거면 else를 써줘야한다 !
+    else:
+        for i in range(N):
+            # (x, i)에 퀸을 놓기
+            row[idx] = i
+            if is_possible(idx):
+                find_Queen(idx + 1)
 
 
 N = int(input())
 ans = 0
-find_Queen(0, 0, 0, [(0, 0)])
+row = [0] * N
+find_Queen(0)
 print(ans)
