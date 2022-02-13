5b00ecc3dfdbaf3cb1aacd07004b351c00e80027
kwanggyo <dlrhkdry29@gmail.com>
2022-02-13 23:54:37 +0900

    BOJ 11726 : 2xn 타일링 : DP

--- Study/기초학습/DP/BOJ_11726_2xn타일링.py
+++ Study/기초학습/DP/BOJ_11726_2xn타일링.py
@@ -1,5 +1,10 @@
 # https://www.acmicpc.net/problem/11726
 # DP
+# 30864KB, 72ms
 # 손으로 그려보면서 점화식 찾기..?
 
 N = int(input())
+dp = [0, 1, 2]
+for i in range(3, 1001):
+  dp.append(dp[i - 2] + dp[i - 1])
+print(dp[N] % 10007)
