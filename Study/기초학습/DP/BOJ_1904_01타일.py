38178d1dd02caabccbac5765a3f73900022c314c
kwanggyo <dlrhkdry29@gmail.com>
2022-03-13 22:26:59 +0900

    BOJ 1904 : 01타일 : DP

---
+++ Study/기초학습/DP/BOJ_1904_01타일.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/1904
+# DP
+
+import sys
+input = sys.stdin.readline
+N = int(input())
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+for k in range(3, N + 1):
+    dp[k] = (dp[k - 1] + dp[k - 2]) % 15746
+
+print(dp[N])
