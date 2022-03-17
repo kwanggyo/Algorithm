43614ace06da3f60665ae46de14c0bc1d80743b4
kwanggyo <dlrhkdry29@gmail.com>
2022-03-17 23:57:43 +0900

    BOJ 14431 : 소수마을 Dijkstra

--- Study/기초학습/다익스트라/BOJ_14431_소수마을.py
+++ Study/기초학습/다익스트라/BOJ_14431_소수마을.py
@@ -2,6 +2,7 @@
 # Dijkstra
 # 다익스트라 + 소수 판별 함수 + 좌표 사이 거리 계산 함수
 
+# 시작점에서 갈 수 있는지 구한다.
 from heapq import heappush, heappop
 from collections import deque
 import sys
@@ -9,6 +10,7 @@ input = sys.stdin.readline
 
 # 두점 사이의 거리를 구하는 함수
 def dist_cal(x1, y1, x2, y2):
+    print((x1-x2)**2, (y1-y2)**2)
     value = int(((x1-x2)**2 + (y1-y2)**2)**0.5)
     return value
 
@@ -24,6 +26,7 @@ def isPrimeNumber(x):
 def BFS(x, y):
     HQ = []
     heappush(HQ, (0, x, y))
+    dist = [INF] * (N + 1)
     while HQ:
         dist, r, c = heappop(HQ)
         if r == er and c == ec:
@@ -32,6 +35,7 @@ def BFS(x, y):
             new_dist = dist_cal(r, c, new_r, new_c)
             print('new_dist', new_dist)
             if isPrimeNumber(new_dist):
+                if 
                 heappush(HQ, (dist + new_dist, new_r, new_c))
     return -1
 
